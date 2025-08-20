7c8b01614245790cebea097a701cbb28e88c7107
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-08-20 15:36:03 +0530

    Add files via upload

---
+++ SpringBoot_CRUD_Day2/src/main/java/com/tka/dao/StudentDao.java
@@ -0,0 +1,29 @@
+package com.tka.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.tka.entites.Student;
+
+@Repository
+public class StudentDao {
+
+	@Autowired
+	SessionFactory factory;
+
+	public String insertData(Student s) {
+
+		Session ss = factory.openSession();
+		Transaction tr = ss.beginTransaction();
+		ss.persist(s);
+		
+		tr.commit();
+		ss.close();
+		
+		return "Data is inserted...";
+	}
+
+}
